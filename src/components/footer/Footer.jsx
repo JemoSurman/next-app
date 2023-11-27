713ac9882042d12f740c4d82205815f5dbcb8e84
jemalisurmanidze01 <87073679+jemalisurmanidze01@users.noreply.github.com>
2023-11-28 00:28:31 +0400

    Adding rogor khar text

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,6 +6,7 @@ function Footer() {
   return (
     <div className={styles.container}>
       {/* {gamarjoba} */}
+      {/* {rogor khar} */}
       <div>@all rights reserved</div>
       <div className={styles.socialNetworks}>
         <Image src="/1.png" width={15} className={styles.socialIcon} height={15} alt="facebook " />
