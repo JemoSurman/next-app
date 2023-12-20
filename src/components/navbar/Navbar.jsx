8d5b6ec02f3783b10829583eaf2b69cd71dd59bb
jemalisurmanidze01 <87073679+jemalisurmanidze01@users.noreply.github.com>
2023-12-21 00:17:55 +0400

    change Category_and_Darkmode

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from './navbar.module.css'
 import Link from "next/link";
+import DarkModeToggle from "../darkModeToggle/DarkModeToggle";
 
 
 function Navbar() {
@@ -40,8 +41,11 @@ function Navbar() {
   ]
   return (
     <div className={styles.container}>
-      <Link href="/" className={styles.logo}>Lamamia</Link>
+      <Link href="/" className={styles.logo}>
+        Lamamia
+      </Link>
       <div className={styles.links}>
+        <DarkModeToggle/>
         {links.map((link) => (
         <Link href={link.url}>{link.title}</Link>
         ))}
